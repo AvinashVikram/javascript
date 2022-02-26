523ac86e62b393895b7032ad345d5668c479241b
AvinashVikram <99747350+AvinashVikram@users.noreply.github.com>
2022-02-26 04:23:40 -0500

    Create index.js

---
+++ EmployeeFilter/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import EmployeeDetail from './Components/EmployeeDetail';
+
+ReactDOM.render(
+    <div>
+        <EmployeeDetail></EmployeeDetail>    
+    </div>
+    ,document.getElementById("root")
+)
